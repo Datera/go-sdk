9c54153ebc572993487394080982a9a240ae2c3f
Tomas Celaya <tomas.celaya@joyent.com>
2020-01-23 13:58:10 -0800

    Add OperationPath field returned when cloning app instance through remote provider

--- pkg/dsdk/app_instances.go
+++ pkg/dsdk/app_instances.go
@@ -22,6 +22,7 @@ type AppInstance struct {
 	Id                      string                  `json:"id,omitempty" mapstructure:"id"`
 	Name                    string                  `json:"name,omitempty" mapstructure:"name"`
 	OpState                 string                  `json:"op_state,omitempty" mapstructure:"op_state"`
+	OperationPath           string                  `json:"operation_path,omitempty" mapstructure:"operation_path"`
 	Path                    string                  `json:"path,omitempty" mapstructure:"path"`
 	RemoteRestorePercentage int                     `json:"remote_restore_percentage,omitempty" mapstructure:"remote_restore_percentage"`
 	RemoteRestoreProgress   string                  `json:"remote_restore_progress,omitempty" mapstructure:"remote_restore_progress"`
